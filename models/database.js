8381369021e05c279e47acdd06d20d63c2b2ce2d
Ulan Sulaimanov <ulansulamaovulan123@gmail.com>
2021-09-28 13:48:16 +0300

    Fix: fix database connection string

--- models/database.js
+++ models/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('mysql://keiz:keiz123@34.133.40.93:3306/solid_back');
+const sequelize = new Sequelize('mysql://root:keiz123@34.66.211.43:3306/solid_back');
 
 module.exports = sequelize;
 
